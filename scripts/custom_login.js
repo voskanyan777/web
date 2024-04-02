233e125a67cb7a59a6e5861e09e7c8d6bb59daa4
voskanyan777 <159440969+voskanyan777@users.noreply.github.com>
2024-04-02 21:56:25 +0700

    added custom login page

--- scripts/custom_login.js
+++ scripts/custom_login.js
@@ -1,11 +1,13 @@
+const login = document.getElementById('login')
+const password = document.getElementById('password')
 const haveAccountBtn = document.getElementById('haveAccountBtn');
 const email = document.getElementById('email')
 const age = document.getElementById('age')
-
 const toggleInput = document.getElementById('toggleInput')
 const divTest = document.querySelector('div.tests')
 const containerDiv = document.querySelector('div.container-div')
-
+const form = document.getElementById('mainForm');
+const signUp = document.getElementById('mainSubmit')
 toggleInput.addEventListener('change', function(){
     if (this.checked){
         age.disabled = true;
@@ -22,22 +24,43 @@ haveAccountBtn.addEventListener('click', function(){
     age.style.display = 'none';
     haveAccountBtn.style.display = 'none';
     divTest.style.display = 'none';
+    mainSubmit.style.display = 'none';
 
-    containerDiv.innerHTML += '<input type="button" value="login" style="margin-left: 1px;" id="loginBtn">'
-    containerDiv.innerHTML += '<input type="button" value="registration" style="margin-left: 1px;" id="registrationBtn">'
-
-
-    const registrationBtn = document.getElementById('registrationBtn')
-    registrationBtn.addEventListener('click', function(){
-        email.style.display = 'block';
-        age.style.display = 'block';
-        haveAccountBtn.style.display = 'block';
-        divTest.style.display = 'block';
-        console.log()
-        
+    containerDiv.innerHTML += '<input type="submit" value="login" style="width: 100%;" id="loginBtn">'
     
-    })
-    
-
+    const loginBtn = document.getElementById('loginBtn')
+    loginBtn.addEventListener('click', function(){
+        console.log('text')
+    })    
 })
 
+
+form.addEventListener('submit', function(event){
+    // Предотвратить отправку формы по умолчанию
+    event.preventDefault();
+    if ((login.value == '' || password.value == '' || email.value =='') && (toggleInput.checked)){
+        console.log('Присутствуют пустые поля')
+        signUp.style.backgroundColor = 'red';
+    }
+    if ((login.value == '' || password.value == '' || email.value =='' || age.value == '') && (!toggleInput.checked)){
+        console.log('Присутствуют пустые поля')
+        signUp.style.backgroundColor = 'red';
+    }
+    if ((!toggleInput.checked) && (age.value < 1)){
+        console.log('Возраст не может быть меньше 1')
+        signUp.style.backgroundColor = 'red';
+    }
+    else{
+        signUp.style.backgroundColor = 'green';
+        const resultJson = {
+            login: login.value,
+            password: password.value,
+            email: email.value,
+        }
+        if (age.value){
+            resultJson['age'] = age.value
+        }
+        console.log(resultJson)
+    }
+    
+})
--- scripts/login.js
+++ scripts/login.js
@@ -1,11 +1,7 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+function some(){
+    console.log('he')
+}
+const mainButton = document.getElementById('mainbtn')
+mainButton.addEventListener('click', function(){
+    mainButton.value = '12'
+})
