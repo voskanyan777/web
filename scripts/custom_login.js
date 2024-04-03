d5174e0ee503c936b19603eaef2e5dfa82b0757c
voskanyan777 <159440969+voskanyan777@users.noreply.github.com>
2024-04-03 20:20:49 +0700

    added jquery for login page

--- scripts/custom_login.js
+++ scripts/custom_login.js
@@ -1,66 +1,106 @@
-const login = document.getElementById('login')
-const password = document.getElementById('password')
-const haveAccountBtn = document.getElementById('haveAccountBtn');
-const email = document.getElementById('email')
-const age = document.getElementById('age')
 const toggleInput = document.getElementById('toggleInput')
-const divTest = document.querySelector('div.tests')
-const containerDiv = document.querySelector('div.container-div')
-const form = document.getElementById('mainForm');
-const signUp = document.getElementById('mainSubmit')
+
+
 toggleInput.addEventListener('change', function(){
     if (this.checked){
-        age.disabled = true;
-        age.value = ''
+        $('#age').prop('disabled', true)
+        $('#age').val('')
     }
     else{
-        age.disabled = false;
+        $('#age').prop('disabled', false)
     }
 })
 
+$("#haveAccountBtn").click(function(event){
+    $('#email').css({'display': 'none'})
+    $('#age').css({'display': 'none'})
+    $('#haveAccountBtn').css({'display': 'none'})
+    $('div.tests').css({'display': 'none'})
+    $('#mainSubmit').css({'display': 'none'})
+    $('div.container-div').append('<input type="submit" value="login" style="width: 100%;" id="loginBtn">')
 
-haveAccountBtn.addEventListener('click', function(){
-    email.style.display = 'none';
-    age.style.display = 'none';
-    haveAccountBtn.style.display = 'none';
-    divTest.style.display = 'none';
-    mainSubmit.style.display = 'none';
-
-    containerDiv.innerHTML += '<input type="submit" value="login" style="width: 100%;" id="loginBtn">'
-    
-    const loginBtn = document.getElementById('loginBtn')
-    loginBtn.addEventListener('click', function(){
-        console.log('text')
-    })    
 })
 
+$('form').submit(function(event){
+    event.preventDefault()
+    const login = $('#login').val()
+    const password = $('#password').val()
+    const email = $('#email').val()
 
-form.addEventListener('submit', function(event){
-    // Предотвратить отправку формы по умолчанию
-    event.preventDefault();
-    if ((login.value == '' || password.value == '' || email.value =='') && (toggleInput.checked)){
+    if ((login == '' || password == '' || email =='') && (toggleInput.checked)){
         console.log('Присутствуют пустые поля')
-        signUp.style.backgroundColor = 'red';
+        $('#mainSubmit').css({'backgroundColor': 'red'})
     }
-    if ((login.value == '' || password.value == '' || email.value =='' || age.value == '') && (!toggleInput.checked)){
+    if ((login == '' || password == '' || email =='' || age == '') && (!toggleInput.checked)){
         console.log('Присутствуют пустые поля')
-        signUp.style.backgroundColor = 'red';
+        $('#mainSubmit').css({'backgroundColor': 'red'})
     }
     if ((!toggleInput.checked) && (age.value < 1)){
         console.log('Возраст не может быть меньше 1')
-        signUp.style.backgroundColor = 'red';
+        $('#mainSubmit').css({'backgroundColor': 'red'})
+    }
+    if ((login.length < 6) || (password.length < 6)){
+        console.log('Логин и пароль не могут быль меньше 6 символов')
+        $('#mainSubmit').css({'backgroundColor': 'red'})
     }
     else{
-        signUp.style.backgroundColor = 'green';
+        $('#mainSubmit').css({'backgroundColor': 'green'})
         const resultJson = {
-            login: login.value,
-            password: password.value,
-            email: email.value,
+            login: login,
+            password: password,
+            email: email,
         }
         if (age.value){
             resultJson['age'] = age.value
         }
         console.log(resultJson)
     }
-    
 })
+
+// form.addEventListener('submit', function(event){
+//     // Предотвратить отправку формы по умолчанию
+//     event.preventDefault();
+//     if ((login.value == '' || password.value == '' || email.value =='') && (toggleInput.checked)){
+//         console.log('Присутствуют пустые поля')
+//         signUp.style.backgroundColor = 'red';
+//     }
+//     if ((login.value == '' || password.value == '' || email.value =='' || age.value == '') && (!toggleInput.checked)){
+//         console.log('Присутствуют пустые поля')
+//         signUp.style.backgroundColor = 'red';
+//     }
+//     if ((!toggleInput.checked) && (age.value < 1)){
+//         console.log('Возраст не может быть меньше 1')
+//         signUp.style.backgroundColor = 'red';
+//     }
+//     else{
+//         signUp.style.backgroundColor = 'green';
+//         const resultJson = {
+//             login: login.value,
+//             password: password.value,
+//             email: email.value,
+//         }
+//         if (age.value){
+//             resultJson['age'] = age.value
+//         }
+//         console.log(resultJson)
+//     }
+    
+// })
+
+
+// haveAccountBtn.addEventListener('click', function(){
+//     email.style.display = 'none';
+//     age.style.display = 'none';
+//     haveAccountBtn.style.display = 'none';
+//     divTest.style.display = 'none';
+//     mainSubmit.style.display = 'none';
+
+//     containerDiv.innerHTML += '<input type="submit" value="login" style="width: 100%;" id="loginBtn">'
+    
+//     const loginBtn = document.getElementById('loginBtn')
+//     loginBtn.addEventListener('click', function(){
+//         console.log('text')
+//     })    
+// })
+
+
