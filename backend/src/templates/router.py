368ebf0434864030bc0d3acd13473662fd419096
voskanyan777 <159440969+voskanyan777@users.noreply.github.com>
2024-04-16 23:07:38 +0700

    fixed bug

--- backend/src/templates/router.py
+++ backend/src/templates/router.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 from fastapi import APIRouter
 from fastapi import Request
 from fastapi.responses import HTMLResponse
@@ -12,12 +13,12 @@ router = APIRouter(
 BASE_DIR = Path(__file__).parent
 print(BASE_DIR)
 
-
 templates = Jinja2Templates(directory=str(BASE_DIR))
 
 
-@router.get("/test", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="base.html"
-    )
+@router.get("/product_description/{product_name}", response_class=HTMLResponse)
+async def read_item(request: Request, product_name: str) -> dict:
+    # return templates.TemplateResponse(
+    #     request=request, name="description.html"
+    # )
+    return templates.TemplateResponse("description.html", {"request": request})
